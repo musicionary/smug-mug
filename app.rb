bbff551e333be9abfe06a0f8ba6353a923b39b2b
Chip Carnes and Noah Ramey <noah@>
2016-07-26 09:33:26 -0700

    add basic routing

--- app.rb
+++ app.rb
@@ -5,3 +5,48 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/') do
   erb(:index)
 end
+
+get('/customer/:id') do
+  @customer = Customer.find(params[:id])
+  erb(:customer)
+end
+
+get('/roasters') do
+  @roasters = Roaster.all()
+  erb(:roasters)
+end
+
+get('/roaster/:id') do
+  @roaster = Roaster.find(params[:id])
+  erb(:roaster)
+end
+
+get('/farmers') do
+  @farmers = Farmer.all()
+  erb(:farmers)
+end
+
+get('/farmer/:id') do
+  @farmer = Farmer.find(params[:id])
+  erb(:farmer)
+end
+
+get('/roasts') do
+  @roasts = Roast.all()
+  erb(:roasts)
+end
+
+get('/roasts/:id') do
+  @roast = Roast.find(params[:id])
+  erb(:roast)
+end
+
+get('/crops') do
+  @crops = Crop.all()
+  erb(:crops)
+end
+
+get('/crops/:id') do
+  @crop = Crop.find(params[:id])
+  erb(:crop)
+end
